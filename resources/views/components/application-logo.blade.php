846745aef34a80f6a1c71f3d45c7bdb30ce7de24
manumengo71 <mcmengo2004@gmail.com>
2026-03-06 21:53:33 +0100

    Fix logo.

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="https://i.postimg.cc/4yKH00Nd/9f3469e4-2cf4-44e0-bf77-bc28b015f363.jpg" class="w-20 h-20 rounded-full" />
+<img src="https://i.postimg.cc/4yKH00Nd/9f3469e4-2cf4-44e0-bf77-bc28b015f363.jpg" {!! $attributes->merge(['class' => '']) !!} />
